ef9bbd34775d9ea4c21874bb5a654c1abc37ca0e
konika098 <konikakhan2000@gmail.com>
2024-11-03 18:53:46 +0600

    add footer

--- src/components/Shared/Footer/index.tsx
+++ src/components/Shared/Footer/index.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import { navItems } from "@/utils/data";
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
-    return (
+  return (
+    <div className="footer">
+      <div className="container">
         <div>
-            hi
+          <h1>Style Mela</h1>
+          <p>(319) 555-0115</p>
+          <p>info@beauty.com</p>
+          <p>4517 Washington Ave. Manchester, Kentucky 39495</p>
+          <h1>We Are Looking Forward To Your Visit!</h1>
+          <hr />
+
+          <ul className="flex space-x-8">
+          {navItems?.map((nav, index) => (
+            <li key={index}>
+              <Link
+                href={nav.path}
+                className="text-black hover:text-gray-600 transition-colors"
+              >
+                {nav.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
         </div>
-    );
+        <div>
+            <h1>©2023 Beauty All Rights are reserved️ </h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
