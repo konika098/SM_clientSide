0ef121254289eaa242b735a9f07418ff578f29f9
konika098 <konikakhan2000@gmail.com>
2024-11-25 17:17:00 +0600

    test

--- src/app/(website)/shop/page.tsx
+++ src/app/(website)/shop/page.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Sidebar from './Sidebar';
-import ProductGrid from './ProductGrid';
-import { products } from '@/utils/data'; 
+// import ProductGrid from './ProductGrid';
+// import { products } from '@/utils/data'; 
 
 const Shop = () => {
   return (
     <div className="shop">
       <div className="container flex">
         <Sidebar />
-        <ProductGrid products={products} />
+        {/* <ProductGrid products={products} /> */}
       </div>
     </div>
   );
